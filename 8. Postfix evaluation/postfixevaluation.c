c2d2ad26b6c88e39830d7a770bba906e163514af
Atul Biju <141022002+Atul013@users.noreply.github.com>
2024-11-03 21:27:28 +0530

    Update postfixevaluation.c

--- 8. Postfix evaluation/postfixevaluation.c
+++ 8. Postfix evaluation/postfixevaluation.c
@@ -1,36 +1,61 @@
 #include <stdio.h>
-
+#include <ctype.h> // For isdigit
 int evaluatePostfix(char* postfix) {
     int stack[100];
     int top = -1; 
     int i = 0;
 
     while (postfix[i] != '\0') {
-        if (postfix[i] >= '0' && postfix[i] <= '9') {
-            stack[++top] = postfix[i] - '0';
-        } else if (postfix[i] == ' ') {
+        if (postfix[i] == ' ') {
             i++;
             continue;
-        } else {
-            int op2 = stack[top--];
-            int op1 = stack[top--];
-
-            switch (postfix[i]) {
-                case '+':
-                    stack[++top] = op1 + op2;
-                    break;
-                case '-':
-                    stack[++top] = op1 - op2;
-                    break;
-                case '*':
-                    stack[++top] = op1 * op2;
-                    break;
-                case '/':
-                    stack[++top] = op1 / op2;
-                    break;
-            }
         }
-        i++;
+
+        if (isdigit(postfix[i])) {
+            int num = 0;
+            while (isdigit(postfix[i])) {
+                num = num * 10 + (postfix[i] - '0');
+                i++;
+            }
+            stack[++top] = num;  
+            continue;  
+        } 
+
+        if (top < 1) {  
+            printf("Error: Invalid postfix expression.\n");
+            return 0;
+        }
+
+        int op2 = stack[top--];
+        int op1 = stack[top--];
+
+        switch (postfix[i]) {
+            case '+':
+                stack[++top] = op1 + op2;
+                break;
+            case '-':
+                stack[++top] = op1 - op2;
+                break;
+            case '*':
+                stack[++top] = op1 * op2;
+                break;
+            case '/':
+                if (op2 == 0) {
+                    printf("Error: Division by zero.\n");
+                    return 0;
+                }
+                stack[++top] = op1 / op2;
+                break;
+            default:
+                printf("Error: Unsupported operator %c.\n", postfix[i]);
+                return 0;
+        }
+        i++;  
+    }
+
+    if (top != 0) {
+        printf("Error: Invalid postfix expression.\n");
+        return 0;
     }
 
     return stack[top];
@@ -42,10 +67,20 @@ int main() {
     printf("Enter postfix expression: ");
     fgets(postfix, 100, stdin);
 
+    size_t len = 0;
+    while (postfix[len] != '\0') {
+        if (postfix[len] == '\n') {
+            postfix[len] = '\0';
+            break;
+        }
+        len++;
+    }
+
     int result = evaluatePostfix(postfix);
 
-    printf("Result: %d\n", result);
+    if (result || (result == 0 && postfix[0] != '\0')) {  
+        printf("Result: %d\n", result);
+    }
 
     return 0;
 }
-
